183925a2fc13c71f35edb693cb14284e230dc604
SakamotoHiroya <ayorih16@icloud.com>
2024-04-19 21:37:48 +0900

    refactor get_student_info.py

--- get_student_info.py
+++ get_student_info.py
@@ -6,30 +6,45 @@ from nfc.tag.tt3 import BlockCode, ServiceCode, Type3Tag
 from nfc.tag.tt3_sony import FelicaStandard
 
 SYSTEM_CODE = 0xFE00
+SERVICE_CODE_NUMBER = 106
+
+STUDENT_ID_BLOCK_NUMBER = 0x0000 # 学生番号が格納されているランダムサービスのブロック番号
+NAME_BLOCK_NUMBER = 0x0001 # 氏名が格納されているランダムサービスのブロック番号
+
+STUDENT_ID_OFFSET = 2 #学生番号を含むブロックの学生番号の開始位置
+STUDENT_ID_LENGTH = 8 #学生番号の長さ
+
+#学生番号を取得
+def get_student_id(tag: Tag) -> str:
+    service_code = ServiceCode(SERVICE_CODE_NUMBER, 0b001011) # 0b001011:ランダムサービス、認証なし、読み取り専用
+    block_code = BlockCode(STUDENT_ID_BLOCK_NUMBER)
+    
+    raw_id = tag.read_without_encryption([service_code], [block_code]);
+    return raw_id.decode("shift_jis")[STUDENT_ID_OFFSET : STUDENT_ID_OFFSET + STUDENT_ID_LENGTH]
+
+#氏名を取得
+def get_name(tag: Tag) -> str:
+    service_code = ServiceCode(SERVICE_CODE_NUMBER, 0b001011) # 0b001011:ランダムサービス、認証なし、読み取り専用
+    block_code = BlockCode(NAME_BLOCK_NUMBER)
+    
+    raw_name = tag.read_without_encryption([service_code], [block_code]);
+    return raw_name.decode("shift_jis")
+
 
 def on_connect(tag: Tag) -> bool:
     if isinstance(tag, FelicaStandard) and SYSTEM_CODE in tag.request_system_code():
-        print("Felica connected.")
-
         tag.idm, tag.pmm, *_ = tag.polling(SYSTEM_CODE)
 
-        service_code = ServiceCode(106, 0b001011)
+        id = get_student_id(tag);
+        name = get_name(tag);
 
-        id_block_code = BlockCode(0x0000)
-        name_block_code = BlockCode(0x0001)
-
-        raw_id = tag.read_without_encryption([service_code], [id_block_code])
-        raw_name = tag.read_without_encryption([service_code], [name_block_code])
-
-        print("id:   " + raw_id.decode("shift_jis")[2:10]) #デコードした文字列から学生番号を切り出す
-        print("name: " + raw_name.decode("shift_jis"))
+        print("--------------------")
+        print("id:   " + id)
+        print("name: " + name)
     else:
-        print("This card is not Felica.")
+        print("Invalid card")
     return True
-
-def on_release(tag: Tag) -> None:
-    print("Card released.")
 
 with nfc.ContactlessFrontend("usb") as clf:
     while True: 
-        clf.connect(rdwr={"on-connect": on_connect, "on-release": on_release})
+        clf.connect(rdwr={"on-connect": on_connect})
